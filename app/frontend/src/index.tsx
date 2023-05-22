b9e98a0ed3ee517fa6dca10fc200e6b42a0dd888
Pascal Kraus <pascal.kraus@stud.tu-darmstadt.de>
2023-05-22 14:03:07 +0200

    landing page for frontend

--- app/frontend/src/index.tsx
+++ app/frontend/src/index.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { initializeIcons } from "@fluentui/react";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+import "./index.css";
+
+import Layout from "./pages/layout/Layout";
+
+initializeIcons();
+
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+      </Routes>
+    </HashRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
